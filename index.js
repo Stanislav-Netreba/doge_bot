e8a1eacd8d8a7387a3ab9061945942daf355293d
Stanislav2007 <68660880+Stanislav2007@users.noreply.github.com>
2021-05-27 00:05:22 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,74 +1,32 @@
-const { ChartJSNodeCanvas } = require('chartjs-node-canvas');
-const fetch = require("node-fetch");
-const fs = require('fs')
 const { Telegraf } = require('telegraf');
+const config = require('./config.json');
 
-const bot = new Telegraf(token);
+const bot = new Telegraf(config.token);
 
-bot.on('message', ctx=>{
+//functions
 
-    if(ctx.message.text != 'chart') return
+const chart = require('./functions/chart.js');
+const doge_price = require('./functions/price.js')
 
-    const width = 800; //px
-    const height = 400; //px
-    const chartJSNodeCanvas = new ChartJSNodeCanvas({ width, height });
+//
 
-    function ut (timestamp) {
-        var d = new Date(timestamp);
-        timeStampCon = d.getDate() + '.' + (d.getMonth()) + '.' + d.getFullYear();
+const uah = config.doge_in_uah;
+const usd = config.doge_in_usd;
+const rub = config.doge_in_rub;
 
-        return timeStampCon;
+
+bot.on('message', ctx => {
+    let command = ctx.message.text
+    if(!command) return;
+    cmdList = command.split(/\s+/g);
+
+    if(cmdList[0].toLowerCase() == 'doge'){
+        let num = +cmdList[1] || 1;
+        doge_price(ctx, num, uah, usd, rub)
     };
 
-    fetch('https://api.coingecko.com/api/v3/coins/dogecoin/market_chart?vs_currency=usd&days=14&interval=daily')
-    .then(res => res.json())
-    .then(json => {
-        var prices = [];
-        var dates = [];
-        json.prices.forEach(element => { 
-            prices.push(element[1]) 
-        });
-        json.prices.forEach(element => { 
-            dates.push(ut(element[0])) 
-        });
-        (async () => {
-            const configuration = {
-                type: 'line',
-                data: {
-                    labels: dates,
-                    datasets: [{
-                        label: 'Doge price',
-                        data: prices,
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 256)'
-                        ],
-                        borderColor: [
-                            'rgba(255,99,132,256)'
-                        ],
-                        borderWidth: 1
-                    }]
-                },
-                plugins:  {
-                    id: 'custom_canvas_background_color',
-                    beforeDraw: (chart) => {
-                    const ctx = chart.canvas.getContext('2d');
-                    ctx.save();
-                    ctx.globalCompositeOperation = 'destination-over';
-                    ctx.fillStyle = 'lightGreen';
-                    ctx.fillRect(0, 0, chart.width, chart.height);
-                    ctx.restore();
-                    }
-                } 
-                
-            };
-            const image = await chartJSNodeCanvas.renderToBuffer(configuration);
-            
-            fs.writeFileSync(`./tmp.png`, image)
-        }) (); 
-    });
-    ctx.replyWithPhoto({
-        source: './tmp.png'
-    })
+    if(cmdList[0].toLowerCase() == 'chart') {
+        chart(ctx, config.doge_price_chart);
+    };
 });
-
 bot.launch();
