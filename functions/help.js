78282defed624f24d54409959174220a41e79fab
Molly_Blanket <68660880+MollyBlanket@users.noreply.github.com>
2021-05-30 21:00:32 +0300

    Add files via upload

---
+++ functions/help.js
@@ -0,0 +1,10 @@
+module.exports = async (bot, ctx) => {
+    ctx.replyWithMarkdown(
+`
+**doge <количество>** - Текущий курс dogecoin, количество писать не обязательно;
+**chart** - График курса dogecoin за сутки;
+
+Поддержать бедного и юного програмиста собаками - DNbwscEk1etVUUtBghu5xLEWj2ghQPtVVp
+`
+    );
+};
